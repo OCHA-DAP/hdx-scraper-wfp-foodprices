3b58b8a223cee30b906ac829d78b502ea4507fba
Mike <rans@email.com>
2019-09-05 13:40:53 +0200

    Handle problem with Philippines JSON

--- wfpfood.py
+++ wfpfood.py
@@ -371,7 +371,7 @@ def joint_dataframe(wfpfood_url, downloader, countriesdata):
             df_country = read_dataframe(wfpfood_url, downloader, countrydata)
         except JSONDecodeError:
             logger.exception('Error with %s! URL: %s' % (wfpfood_url, countryname))
-            return None, None
+            continue
 
         df_country = df_country.loc[1:]
 
